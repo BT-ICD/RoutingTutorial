26a9de78ab76beba04a63c670334711a9c8cd13b
Bhavin Trivedi <dearbhavin@hotmail.com>
2020-11-02 21:34:58 +0530

    Pass complex object with routing using state

--- src/app/team/detail/team-member-detail.component.ts
+++ src/app/team/detail/team-member-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ITeamMember } from '../iteam-member';
 import { TeamMemberDataService } from '../team-member-data.service';
 
@@ -13,19 +13,28 @@ teamMember:ITeamMember;
 id:number;
 name:string;
 mobile:string;
-  constructor(private route:ActivatedRoute, private teamMemberDataService:TeamMemberDataService) { }
+
+  constructor(private route:ActivatedRoute, private teamMemberDataService:TeamMemberDataService, private router:Router) {
+    //Read value of state object - using NavigationExtra - This is available in constructor - cannot available once routing completed or available as history.state
+    //Reference - https://rangle.io/blog/routing-features-angular-7-2-0/ 
+    //Referfence - https://medium.com/ableneo/how-to-pass-data-between-routed-components-in-angular-2306308d8255
+    let stateobj = this.router.getCurrentNavigation().extras.state;
+    console.log(this.router.getCurrentNavigation());
+    console.log(stateobj);
+    
+   }
 
   ngOnInit(): void {
     let teams:ITeamMember[]
+    //read values from query string parameters
     this.route.paramMap.subscribe(params=>{
       this.id = +params.get('id');
       this.name = params.get('name');
       this.mobile=params.get('mobile');
 
     });
-    //read values from query string parameters
-    
-
+    //To get details of state object passed during routing
+    console.log(history.state);
     teams= this.teamMemberDataService.getTeamMembers().filter(obj=>obj.id==this.id);
     console.log(teams);
     if(teams.length==1){
