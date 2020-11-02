375dc32f12da870af6c80f23bd8c6f00e9faafc5
Bhavin Trivedi <dearbhavin@hotmail.com>
2020-11-02 21:56:09 +0530

    Added Routing Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationCancel, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RoutingTutorial';
+  
+  constructor(private router:Router) {
+    this.router.events.subscribe((routerEvent:Event)=>{
+    this.checkRouterEvent(routerEvent);
+    });
+    
+  }
+  checkRouterEvent(routerEvent:Event){
+    if(routerEvent instanceof NavigationStart){
+      console.log('Event : NavigationStart');
+    }
+    else if (routerEvent instanceof NavigationCancel){
+      console.log('Event : NavigationCancel');
+    }
+    else if (routerEvent instanceof NavigationEnd){
+      console.log('Event : NavigationEnd');
+    }
+    else if (routerEvent instanceof NavigationError){
+      console.log('Event : NavigationError');
+    }
+  }
 }
